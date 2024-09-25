f239489fae4b366375e25885c9b3efdab0588604
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-09-25 10:20:50 +0200

    poeomdto

--- src/main/java/app/DTOs/PoemDTO.java
+++ src/main/java/app/DTOs/PoemDTO.java
@@ -1,5 +1,16 @@
 package app.DTOs;
 
+import app.entities.Poem;
+import lombok.Data;
+
+@Data
+
 public class PoemDTO
 {
+    private Poem poem;
+
+    public PoemDTO(Poem poem)
+    {
+        this.poem = poem;
+    }
 }
